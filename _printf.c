6f9793bed1d2ba3203d73e6e5a8c4d0ecdb7de5e
OUSSAMA HATTAN <ossamahattan@gmail.com>
2023-10-18 02:55:08 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,52 +1,52 @@
 #include "main.h"
 
 /**
- * format_specifier - my printf function.
- * @format: format to a string.
- * @i: hello.
- * @len: hello.
- * @args: hello.
- * Return: length of printed a string, or -1 if an error.
+ * format_specifier - Handle format specifiers for printf.
+ * @format: The format string.
+ * @i: The current index in the format string.
+ * @args: The va_list containing arguments.
+ * @len: Pointer to the length of printed string.
+ * Return: 1 if a format specifier was matched, 0 otherwise.
  */
 int format_specifier(const char *format, int *i, va_list args, int *len)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char}, {"%%", printf_37},
-		{"%d", printf_integer}, {"%i", printf_integer}, {"%b", printf_binary},
-		{"%u", printf_unsigned}, {"%o", printf_octal}, {"%x", printf_hexadecimal},
-		{"%X", printf_upper_hexadecimal}, {"%S", printf_S}, {"%p", printf_pointer},
-		{"%r", printf_revstr}, {"%R", printf_rot13}
-	};
+        convert_match m[] = {
+                {"%s", printf_string}, {"%c", printf_char}, {"%%", printf_37},
+                {"%d", printf_integer}, {"%i", printf_integer}, {"%b", printf_binary},
+                {"%u", printf_unsigned}, {"%o", printf_octal}, {"%x", printf_hexadecimal},
+                {"%X", printf_upper_hexadecimal}, {"%S", printf_S}, {"%p", printf_pointer},
+                {"%r", printf_revstr}, {"%R", printf_rot13}
+        };
 
-	int found_match = 0, j;
-	char n_ch;
+        int found_match = 0, j;
+        char n_c;
 
-	for (j = 0; j < 14; j++)
-	{
-		n_ch = format[*i + 1];
-		if (n_ch == ' ' || n_ch == '+' || n_ch == '#' || n_ch == '0' || n_ch == '-')
-			n_ch = format[*i + 2];
+        for (j = 0; j < 14; j++)
+        {
+                n_c = format[*i + 1];
+                if (n_c == ' ' || n_c == '+' || n_c == '#' || n_c == '0' || n_c == '-')
+                        n_c = format[*i + 2];
 
-		if (format[*i] == '%' && n_ch && n_ch == m[j].id[1])
-		{
-			*len += m[j].f(args);
-			found_match = 1;
-			break;
-		}
-	}
+                if (format[*i] == '%' && n_c && n_c == m[j].id[1])
+                {
+                        *len += m[j].f(args);
+                        found_match = 1;
+                        break;
+                }
+        }
 
-	if (found_match)
-	{
-		n_ch = format[*i + 1];
-		if (n_ch == '+' || n_ch == ' ' || n_ch == '#' || n_ch == '0' || n_ch == '-')
-			*i += 3;
-		else
-			*i += 2;
+        if (found_match)
+        {
+                n_c = format[*i + 1];
+                if (n_c == '+' || n_c == ' ' || n_c == '#' || n_c == '0' || n_c == '-')
+                        *i += 3;
+                else
+                        *i += 2;
 
-		return (1);
-	}
+                return (1);
+        }
 
-	return (0);
+        return (0);
 }
 
 /**
